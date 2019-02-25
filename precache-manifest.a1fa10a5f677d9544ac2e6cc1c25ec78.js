09452167223f9de38b996717a75e2a9111a3260c
Minh Nguyen <thanhnhatminhnguyen@gmail.com>
2019-02-25 15:22:32 -0800

    Updates

---
+++ precache-manifest.a1fa10a5f677d9544ac2e6cc1c25ec78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "646a54a103eec3060141",
+    "url": "/React-WouldYouRather/static/js/runtime~main.646a54a1.js"
+  },
+  {
+    "revision": "570956eeedb779afc04f",
+    "url": "/React-WouldYouRather/static/js/main.570956ee.chunk.js"
+  },
+  {
+    "revision": "2e8b455ec0740309b68f",
+    "url": "/React-WouldYouRather/static/js/2.2e8b455e.chunk.js"
+  },
+  {
+    "revision": "570956eeedb779afc04f",
+    "url": "/React-WouldYouRather/static/css/main.f69d1926.chunk.css"
+  },
+  {
+    "revision": "9f3f2e2cad12a3f8ac3598409cd47df7",
+    "url": "/React-WouldYouRather/index.html"
+  }
+];
